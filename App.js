11ce70e47dcb6c8980b39bfaffe5ae2c5bdff6b7
Leonam Quintão <leonamquintao@gmail.com>
2018-12-21 15:52:12 -0200

    refactoring for a Landing component

--- App.js
+++ App.js
@@ -1,51 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Platform, View, Image, Text, Button } from 'react-native';
-
-var backgroundImage = require('./src/assets/img/landing.jpg');
+import Landing from './src/pages/Landing';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={ styles.container }>
-        <Image source={ backgroundImage } style={styles.bkContainer} />
-          <View style={ styles.innerView }>
-            <Text style={ styles.title }>Welcome to Pokesearch </Text>
-            <Button onPress={ this.redirect.bind(this) } title="Enter Here!" color="#3544a3" />
-          </View>
-      </View>
+      <Landing />
     );
-  }
-
-  /* METHODS */
-  redirect() {
-    alert('Clicked Here!!!')
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Platform.OS === 'android' ? 24: 4
-  },
-  innerView: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 30,
-    color: '#3544a3',
-    alignItems: 'center' 
-  },
-  bkContainer: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0, 
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
-  }
-});
