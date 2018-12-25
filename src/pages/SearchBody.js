ce605f422170070ebddf8062df19e0e5cadb175d
Leonam Quintão <leonamquintao@gmail.com>
2018-12-25 14:58:45 -0200

    add pokemon moves and style

--- src/pages/SearchBody.js
+++ src/pages/SearchBody.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Image, Text, ScrollView, StyleSheet } from 'react-native';
+import { List, ListItem } from 'native-base';
 
 export default class SearchBody extends Component {
   constructor(props) {
@@ -19,6 +20,33 @@ export default class SearchBody extends Component {
         </Text>
         <View style={styles.bodyStyle}>
           <Image style={styles.sprite} source={{ uri: pokemon.sprites.front_default }} />
+        </View>
+
+        <View style={styles.info}>
+
+          <ListItem itemDivider>
+            <Text style={styles.title}>Size: </Text>
+          </ListItem>
+
+          <ListItem>
+            <Text>Weight: {pokemon.weight}Kg</Text>
+          </ListItem>
+
+          <ListItem>
+            <Text>Height: {pokemon.height/10}Mts</Text>
+          </ListItem>
+
+          <ListItem itemDivider>
+            <Text style={styles.title}>Abilities:( Moves {pokemon.moves.length} )</Text>
+          </ListItem>
+
+          <List dataArray={pokemon.abilities} renderRow={(item) =>
+            <ListItem>
+              <Text>{item.ability.name}</Text>
+            </ListItem>
+          }>
+
+          </List>
         </View>
       </ScrollView>
     );
@@ -46,5 +74,13 @@ const styles = StyleSheet.create({
     height: 250,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  info: {
+    flex: 1,
+    backgroundColor: '#fff',
+    opacity: 0.8
+  },
+  title: {
+    fontWeight: 'bold'
   }
 });
